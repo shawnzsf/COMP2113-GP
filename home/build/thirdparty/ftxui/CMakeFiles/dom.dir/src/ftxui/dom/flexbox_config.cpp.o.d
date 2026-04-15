70765dc5c4410b7858129469e8e49fde45d182c5
Shoufeng Zhang <126072296+shawnzsf@users.noreply.github.com>
2026-04-15 12:06:59 +0000

    Remove building related assets

--- home/build/thirdparty/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-thirdparty/ftxui/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /workspaces/COMP2113-GP/home/thirdparty/ftxui/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /workspaces/COMP2113-GP/home/thirdparty/ftxui/include/ftxui/dom/flexbox_config.hpp
